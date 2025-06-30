9a62a45e67f5b51d9dbf75e7d135967cd6950397
Jinxiaolong1129 <inxiaolong1129@gmail.com>
2025-06-30 02:47:48 -0400

    update

--- run/ray_batch_eval.sh
+++ run/ray_batch_eval.sh
@@ -1,3 +1,4 @@
+# NOTE pre align stage
 # 先生成对应的model 文件
 python run/generate_config.py --root_dir /data3/user/jin509/malicious-finetuning/experiments-booster-vaccine-repnoise
 
@@ -8,6 +9,10 @@ CUDA_VISIBLE_DEVICES=0,1,2 python run/ray_batch_eval.py --config /data3/user/jin
 CUDA_VISIBLE_DEVICES=0,1,2 python run/ray_batch_eval.py --config /data3/user/jin509/malicious-finetuning/experiments-booster-vaccine-repnoise/lm_eval_model_configs_pre_alignment.json --tasks all --num-gpus 1 --progress-file /data3/user/jin509/malicious-finetuning/experiments-sft_stage/lm_eval_model_configs_pre_alignment_progress.csv
 
 
+
+
+
+# NOTE SFT stage
 python run/generate_config.py --root_dir /data3/user/jin509/malicious-finetuning/experiments-sft_stage
 # final model 评测
 # 评估"mmlu", "humaneval", "gsm8k", "truthfulqa_mc1", "truthfulqa_mc2"
